8ae41ed26efbc41fccb19654f9caf9b793e1df43
wodyd648 <harry6480310@gmail.com>
2022-08-24 15:45:31 +0900

    update

---
+++ pystudy_1/함수 = 코드의 집합.py
@@ -0,0 +1,42 @@
+def print_n_times(value, n):
+    for i in range(n):
+        print(value)
+
+print_n_times("안녕하세요", 5)
+
+
+def print_n_times(n, *value)
+    for i in range(n):
+        for value in values:
+            print(value)
+        print()
+
+
+print_n_times(3, "안녕하세요", "즐거운", "프로그래밍")
+
+
+def print_n_times(value, n=2):
+    for i in range(n):
+        for value in values:
+            print(value)
+
+
+print_n_times("안녕하세요")
+
+
+def print_n_times(*value, n=2):
+    for i in range(n):
+        for value in values:
+            print(value)
+        print()
+
+
+print_n_times("안녕하세요", "즐거운", "프로그래밍", n = 3)
+
+
+def return_test():
+    print("A위치입니다.")
+    return
+    print("B위치입니다.")
+
+return_test()
